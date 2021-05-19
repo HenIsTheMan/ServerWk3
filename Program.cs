0e8e8cf3371d1baefce2b326bc494f1fea882694
Henry Lim <193541T@mymail.nyp.edu.sg>
2021-05-19 12:06:09 +0800

    23

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿class Program {
 	static void Main(string[] args) {
 		Uni.Database db = new Uni.Database();
-		db.Connect("localhost", 5055, "test_db", "Test Test", "Yes");
+		db.Connect("localhost", 5055, "test_db", "Test Test", "password");
 	}
 }
